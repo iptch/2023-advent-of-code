900ff776b5318be0554d9240ca80326c7598ef9e
Weili Gao <info@weili-gao.com>
2023-12-03 17:46:55 +0100

    WGA: Day 3

---
+++ WGA/03/aoc_template.py
@@ -0,0 +1,89 @@
+# aoc_template.py
+
+import pathlib
+import sys
+import re
+
+REGEX_NUMBERS = r'\d+'
+REGEX_SYMBOLS = r'[^\w\s.]'
+REGEX_GEARS = r'\*'
+
+def extract(regex, line):
+    symbols = []
+
+    for match in re.finditer(regex, line):
+        symbols.append((match.group(), match.start(), match.end()))
+
+    return symbols
+
+def is_adjacent_to_symbol(data, row, col):
+    for i in range(row - 1, row + 2):
+        for j in range(col - 1, col + 2):
+            if (0 <= i < len(data) and 0 <= j < len(data) and (i != row or j != col) and
+                    re.search(REGEX_SYMBOLS, data[i][j]) is not None):
+                return True
+
+    return False
+
+def get_adjacent_numbers(numbers_matrix, row, col):
+    adjacent_numbers = []
+
+    for i in range(row - 1, row + 2):
+        if 0 <= i < len(numbers_matrix):
+            for number in numbers_matrix[i]:
+                if number[1] <= col + 1 and number[2] >= col:
+                    adjacent_numbers.append(int(number[0]))
+
+    return adjacent_numbers
+
+def parse(puzzle_input):
+    """Parse input."""
+    return puzzle_input.split()
+
+def part1(data):
+    """Solve part 1."""
+
+    result = 0
+
+    numbers_matrix = [extract(REGEX_NUMBERS, line) for line in data]
+
+    for i, numbers in enumerate(numbers_matrix):
+        for j in numbers:
+            for col in range(j[1], j[2]):
+                if is_adjacent_to_symbol(data, i, col):
+                    result += int(j[0])
+                    break
+
+    return result
+
+def part2(data):
+    """Solve part 2."""
+
+    result = 0
+
+    numbers_matrix = [extract(REGEX_NUMBERS, line) for line in data]
+    gears_matrix = [extract(REGEX_GEARS, line) for line in data]
+
+    for i, gears in enumerate(gears_matrix):
+        for j in gears:
+            adjacent_numbers = get_adjacent_numbers(numbers_matrix, i, j[1])
+
+            if len(adjacent_numbers) == 2:
+                result += adjacent_numbers[0] * adjacent_numbers[1]
+
+    return result
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+
+    return solution1, solution2
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        print(f"{path}:")
+        puzzle_input = pathlib.Path(path).read_text().strip()
+        solutions = solve(puzzle_input)
+        print("\n".join(str(solution) for solution in solutions))
---
+++ WGA/03/test_aoc_template.py
@@ -0,0 +1,58 @@
+# test_aoc_template.py
+
+import pathlib
+import pytest
+import aoc_template as aoc
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+@pytest.fixture
+def example1():
+    puzzle_input = (PUZZLE_DIR / "example1.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+@pytest.fixture
+def example2():
+    puzzle_input = (PUZZLE_DIR / "example2.txt").read_text().strip()
+    return aoc.parse(puzzle_input)
+
+def test_extract_numbers():
+    """Test that numbers are extracted properly."""
+    assert aoc.extract(aoc.REGEX_NUMBERS, "467..114..") == [("467", 0, 3), ("114", 5, 8)]
+
+def test_extract_gears():
+    """Test that gears are extracted properly."""
+    assert aoc.extract(aoc.REGEX_GEARS, "...*......") == [("*", 3, 4)]
+
+@pytest.mark.skip(reason="Not implemented")
+def test_is_adjacent_to_symbol(example1):
+    """Test that gears are extracted properly."""
+    assert aoc.is_adjacent_to_symbol(example1, 0, 0) == True
+
+def test_parse_example1(example1):
+    """Test that input is parsed properly."""
+    assert example1 == [
+        "467..114..",
+        "...*......",
+        "..35..633.",
+        "......#...",
+        "617*......",
+        ".....+.58.",
+        "..592.....",
+        "......755.",
+        "...$.*....",
+        ".664.598.."
+    ]
+
+def test_part1_example1(example1):
+    """Test part 1 on example input."""
+    assert aoc.part1(example1) == 4361
+
+def test_part2_example1(example1):
+    """Test part 2 on example input."""
+    assert aoc.part2(example1) == 467835
+
+@pytest.mark.skip(reason="Not implemented")
+def test_part2_example2(example2):
+    """Test part 2 on example input."""
+    assert aoc.part2(example2) == ...
