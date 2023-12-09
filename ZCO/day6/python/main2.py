edb9583decd2824337633c4f728938554d00fbf0
Zak Cook <zakcook4@gmail.com>
2023-12-09 15:40:44 +0100

    Improve day 6 solutions from ZCO

--- ZCO/day6/python/main2.py
+++ ZCO/day6/python/main2.py
@@ -17,18 +17,10 @@ record_travel = int(line.removeprefix("Distance:").strip().replace(" ", ""))
 #   root1 = (T + discriminant) / 2
 #   root2 = (T - discriminant) / 2
 discriminant = math.sqrt(T**2 - 4 * record_travel)
-root1 = (T + discriminant) / 2
-root2 = (T - discriminant) / 2
-root1, root2 = sorted([root1, root2])
+root1 = (T - discriminant) / 2
+root2 = (T + discriminant) / 2
 
-# We can then take the amount of distinct values strictly between these two roots
-if int(root1) == root1:
-    start = int(root1) + 1
-else:
-    start = math.ceil(root1)
-if int(root2) == root2:
-    end = int(root2) - 1
-else:
-    end = math.floor(root2)
+start = math.floor(root1) + 1
+end = math.ceil(root2) - 1
 
 print(len(range(start, min(end, T) + 1)))
